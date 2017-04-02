7bee0eae05a80c1b72bc9bef86dc500df40e0f92
Danny Leshem <dleshem@gmail.com>
2017-04-02 15:51:57 +0300

    Fixed runtime issue related to reading resources in Scala.

--- libpay-paypal/src/main/scala/com/wix/pay/paypal/iin/IIN.scala
+++ libpay-paypal/src/main/scala/com/wix/pay/paypal/iin/IIN.scala
@@ -1,14 +1,10 @@
 package com.wix.pay.paypal.iin
 
-import java.io.File
-
 import scala.io.Source
 
 object IIN {
   val countryCodes: Map[String, String] = {
-    val file = new File(getClass.getResource("/iin-countries.csv").toURI)
-
-    Source.fromFile(file).getLines.map { line =>
+    Source.fromInputStream(getClass.getResourceAsStream("/iin-countries.csv")).getLines.map { line =>
       val index = line.indexOf(',')
       val iin = line.substring(0, index)
       val countryCode = line.substring(index + 1)
